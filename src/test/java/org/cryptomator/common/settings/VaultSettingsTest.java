80526604b61c283f7f5b51acb550368138bb06cb
Andrew Clarke <32690715+andrewkyle@users.noreply.github.com>
2024-10-07 15:25:22 -0400

    Add unit tests to org.cryptomator.common.settings/VaultSettings

--- src/test/java/org/cryptomator/common/settings/VaultSettingsTest.java
+++ src/test/java/org/cryptomator/common/settings/VaultSettingsTest.java
@@ -13,6 +13,9 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class VaultSettingsTest {
 
 	@ParameterizedTest(name = "VaultSettings.normalizeDisplayName({0}) = {1}")
@@ -26,6 +29,68 @@ public class VaultSettingsTest {
 	})
 	public void testNormalize(String test, String expected) {
 		assertEquals(expected, VaultSettings.normalizeDisplayName(test));
+	}
+
+	@Test
+	public void testEqualsSameId() {
+		var json1 = new VaultSettingsJson();
+		var json2 = new VaultSettingsJson();
+		json1.id = "some_id";
+		json2.id = "some_id";
+
+		var vaultSettings1 = new VaultSettings(json1);
+		var vaultSettings2 = new VaultSettings(json2);
+
+		// VaultSettings returns true when comparing with
+		// another VaultSettings having the same id value in its JSON.
+		Assertions.assertEquals(vaultSettings1, vaultSettings2);
+	}
+
+	@Test
+	public void testNotEqualsSameId() {
+		var json1 = new VaultSettingsJson();
+		var json2 = new VaultSettingsJson();
+		json1.id = "some_id";
+		json2.id = "another_id";
+
+		var vaultSettings1 = new VaultSettings(json1);
+		var vaultSettings2 = new VaultSettings(json2);
+
+		// VaultSettings returns false when comparing with
+		// another VaultSettings having a different id value in its JSON.
+		Assertions.assertNotEquals(vaultSettings1, vaultSettings2);
+	}
+
+	private class VaultSettings2 extends VaultSettings {
+		VaultSettings2(VaultSettingsJson json) {
+			super(json);
+		}
+	}
+
+	@Test
+	public void testNotEqualsObjectClass1() {
+		var json1 = new VaultSettingsJson();
+		var json2 = new VaultSettingsJson();
+		json1.id = "some_id";
+		json2.id = "some_id";
+
+		var vaultSettings1 = new VaultSettings(json1);
+		var vaultSettings2 = new VaultSettings2(json2);
+
+		// VaultSettings returns false when comparing with a subclass of VaultSettings.
+		Assertions.assertNotEquals(vaultSettings1, vaultSettings2);
+	}
+
+	@Test
+	public void testNotEqualsObjectClass2() {
+		var json1 = new VaultSettingsJson();
+		json1.id = "some_id";
+
+		var vaultSettings1 = new VaultSettings(json1);
+		var randomObject = new String("random string");
+
+		// VaultSettings returns false when comparing with another object class.
+		Assertions.assertNotEquals(vaultSettings1, randomObject);
 	}
 
 }
